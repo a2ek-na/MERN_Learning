f71c0f7cf877720585344b74aba47d6c6e6b1443
Abhishek Gupta <abhishek3908@gmail.com>
2025-08-22 04:08:06 +0530

    .

--- Express JS/Project/Project2 - Sukun Nivas/routes/userRouters.js
+++ Express JS/Project/Project2 - Sukun Nivas/routes/userRouters.js
@@ -2,10 +2,11 @@ const exp = require('express');
 const routers = exp.Router();
 
 routers.get('/', (request, response, next) => {
-    response.end();
+    response.sendFile(path.join(__dirname, "../", "views", "user", "home.html"));
 });
 
 routers.get('/rentHome', (request, response, next) => {
+    response.sendFile(path.join(__dirname, "../", "views", "user", "rentHome.html"));
 });
 
 module.exports = routers;
